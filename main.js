f2c36598ce80c0763bc7ef24312b4721c37c47b3
RyanMazzeu <ryanmazzeu111@gmail.com>
2025-03-14 14:33:53 -0300

    criando espaços vazios

--- main.js
+++ main.js
@@ -6,12 +6,21 @@ var jogador = "o";
 var vencedor = " ";
 var fimJogo = false;
 var tab = [3];
-
-while (jogadas < 9 && fimJogo == false) {
-  lin = parseInt(prompt(`${jogador} em [linha]: `));
-  col = parseInt(prompt(`${jogador} em [coluna]: `));
+//(a)
+for (let i = 0; i < 3; i++) {
+  tab[i] = [];
+  for (let j = 0; j < 3; j++) {
+    tab[i][j] = " ";
+  }
 }
 
+while (jogadas < 9 && fimJogo == false) {
+  //(b)
+  lin = parseInt(prompt(`${jogador} em [linha]: `));
+  col = parseInt(prompt(`${jogador} em [coluna]: `));
+  //(c)
+}
+//(d)
 if (vencedor == " ") {
   console.log("empate\n");
 } else {
