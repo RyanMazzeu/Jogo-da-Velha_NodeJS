cbd9e8f38335fb6bf2e881a09749a4dac6f7ed92
RyanMazzeu <ryanmazzeu111@gmail.com>
2025-03-14 14:58:19 -0300

    Printei o tabuleiro

--- main.js
+++ main.js
@@ -16,6 +16,12 @@ for (let i = 0; i < 3; i++) {
 
 while (jogadas < 9 && fimJogo == false) {
   //(b)
+  for (let i = 0; i < 3; i++) {
+    console.log(
+      "[" + tab[i][0] + "]" + "[" + tab[i][1] + "]" + "[" + tab[i][2] + "]"
+    );
+    for (let j = 0; j < 3; j++) {}
+  }
   lin = parseInt(prompt(`${jogador} em [linha]: `));
   col = parseInt(prompt(`${jogador} em [coluna]: `));
   //(c)
