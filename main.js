d26c5eb6e5ad47eab6e0063fd28565a7a974b3a2
RyanMazzeu <ryanmazzeu111@gmail.com>
2025-03-14 15:12:31 -0300

    #3 Finalizei o jogo

--- main.js
+++ main.js
@@ -1,34 +1,66 @@
 const prompt = require("prompt-sync")();
 
-var i, j, lin, col;
 var jogadas = 0;
 var jogador = "o";
 var vencedor = " ";
 var fimJogo = false;
-var tab = [3];
-//(a)
-for (let i = 0; i < 3; i++) {
-  tab[i] = [];
-  for (let j = 0; j < 3; j++) {
-    tab[i][j] = " ";
+var tab = [
+  [" ", " ", " "],
+  [" ", " ", " "],
+  [" ", " ", " "],
+];
+
+function exibirTabuleiro() {
+  for (let i = 0; i < 3; i++) {
+    console.log(`[${tab[i][0]}][${tab[i][1]}][${tab[i][2]}]`);
   }
 }
 
-while (jogadas < 9 && fimJogo == false) {
-  //(b)
+function verificarVencedor() {
+  // Verifica linhas, colunas e diagonais
   for (let i = 0; i < 3; i++) {
-    console.log(
-      "[" + tab[i][0] + "]" + "[" + tab[i][1] + "]" + "[" + tab[i][2] + "]"
-    );
-    for (let j = 0; j < 3; j++) {}
+    if (tab[i][0] === jogador && tab[i][1] === jogador && tab[i][2] === jogador)
+      return true; // Linhas
+    if (tab[0][i] === jogador && tab[1][i] === jogador && tab[2][i] === jogador)
+      return true; // Colunas
   }
-  lin = parseInt(prompt(`${jogador} em [linha]: `));
-  col = parseInt(prompt(`${jogador} em [coluna]: `));
-  //(c)
+  if (
+    (tab[0][0] === jogador && tab[1][1] === jogador && tab[2][2] === jogador) ||
+    (tab[0][2] === jogador && tab[1][1] === jogador && tab[2][0] === jogador)
+  )
+    return true; // Diagonais
+  return false;
 }
-//(d)
-if (vencedor == " ") {
-  console.log("empate\n");
-} else {
-  console.log(`${vencedor} venceu`);
+
+while (jogadas < 9 && !fimJogo) {
+  exibirTabuleiro();
+  let lin, col;
+
+  while (true) {
+    lin = parseInt(prompt(`${jogador} em [linha]: `));
+    col = parseInt(prompt(`${jogador} em [coluna]: `));
+
+    if (lin < 0 || lin > 2 || col < 0 || col > 2) {
+      console.log("Coordenadas inválidas! Escolha entre 0 e 2.");
+      continue;
+    }
+    if (tab[lin][col] !== " ") {
+      console.log("Posição já ocupada! Escolha outra.");
+      continue;
+    }
+    break;
+  }
+
+  tab[lin][col] = jogador;
+  jogadas++;
+
+  if (verificarVencedor()) {
+    fimJogo = true;
+    vencedor = jogador;
+  } else {
+    jogador = jogador === "o" ? "x" : "o"; // Alterna jogador
+  }
 }
+
+exibirTabuleiro();
+console.log(vencedor === " " ? "Empate!" : `${vencedor} venceu!`);
